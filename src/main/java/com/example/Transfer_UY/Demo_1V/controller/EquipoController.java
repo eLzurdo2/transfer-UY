a3b93ca3650597c261fb53fe9823e13878d8669d
eLzurdo2 <santiagoarnaud2@gamil.com>
2024-11-09 15:42:35 -0300

    second commit

--- src/main/java/com/example/Transfer_UY/Demo_1V/controller/EquipoController.java
+++ src/main/java/com/example/Transfer_UY/Demo_1V/controller/EquipoController.java
@@ -5,17 +5,32 @@ import com.example.Transfer_UY.Demo_1V.repository.EquipoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
+@RequestMapping("/equipo")
 public class EquipoController {
 
     @Autowired
     private EquipoRepository equipoRepository;
 
-    @GetMapping("/equipo")
+
+    @GetMapping
     public String listarEquipos(Model model) {
         model.addAttribute("equipos", equipoRepository.findAll());
         return "equipos";
+    }
+
+    @GetMapping("/nuevo")
+    public String mostrarFormularioCrearEquipo(Model model) {
+        model.addAttribute("equipo", new Equipo());
+        return "crear_equipo";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarEquipo(@ModelAttribute Equipo equipo) {
+        equipoRepository.save(equipo);
+        return "redirect:/equipo";
     }
 }
