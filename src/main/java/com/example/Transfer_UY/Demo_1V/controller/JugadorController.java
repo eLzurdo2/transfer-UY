cdb2ad1ca5e15d4a241874b4b51568268a9c6002
eLzurdo2 <santiagoarnaud2@gamil.com>
2025-01-13 16:45:00 -0300

    commit de transferencia

--- src/main/java/com/example/Transfer_UY/Demo_1V/controller/JugadorController.java
+++ src/main/java/com/example/Transfer_UY/Demo_1V/controller/JugadorController.java
@@ -1,8 +1,6 @@
 package com.example.Transfer_UY.Demo_1V.controller;
 
 import com.example.Transfer_UY.Demo_1V.models.Jugador;
-import com.example.Transfer_UY.Demo_1V.models.Equipo;
-import com.example.Transfer_UY.Demo_1V.models.Transferencia;
 import com.example.Transfer_UY.Demo_1V.repository.EquipoRepository;
 import com.example.Transfer_UY.Demo_1V.repository.JugadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Optional;
-
 @Controller
 public class JugadorController {
 
     @Autowired
     private JugadorRepository jugadorRepository;
+
     @Autowired
     private EquipoRepository equipoRepository;
 
@@ -41,15 +38,6 @@ public class JugadorController {
         return "nuevo_jugador";
     }
 
-    @GetMapping("/transferir")
-    public String mostrarFormularioTransferencia(Model model) {
-        model.addAttribute("equipos", equipoRepository.findAll());
-        model.addAttribute("jugadores", jugadorRepository.findAll());
-        model.addAttribute("transferencia", new Transferencia());
-        return "transferencia";
-    }
-
-
     @PostMapping("/guardar")
     public String guardarJugador(@ModelAttribute Jugador jugador) {
         jugadorRepository.save(jugador);
@@ -57,26 +45,4 @@ public class JugadorController {
 
     }
 
-
-    @PostMapping("/transferir")
-    public String transferirJugador(@ModelAttribute Transferencia transferencia) {
-        Optional<Jugador> jugadorOpt = jugadorRepository.findById(transferencia.getJugadorId());
-        Optional<Equipo> equipoDestinoOpt = equipoRepository.findById(transferencia.getEquipoDestinoId());
-
-        if (jugadorOpt.isPresent() && equipoDestinoOpt.isPresent()) {
-            Jugador jugador = jugadorOpt.get();
-            Equipo equipoDestino = equipoDestinoOpt.get();
-
-            jugador.setEquipo(equipoDestino);
-            jugador.setValorTransferencia(transferencia.getValorTransferencia());
-            jugador.setTipoTransferencia(transferencia.getTipoTransferencia());
-            jugadorRepository.save(jugador);
-
-            return "redirect:/jugadores";
-        } else {
-
-            return "redirect:/error";
-        }
-    }
 }
-
---
+++ src/main/java/com/example/Transfer_UY/Demo_1V/controller/TransferenciaController.java
@@ -0,0 +1,53 @@
+package com.example.Transfer_UY.Demo_1V.controller;
+
+import com.example.Transfer_UY.Demo_1V.models.Jugador;
+import com.example.Transfer_UY.Demo_1V.models.Equipo;
+import com.example.Transfer_UY.Demo_1V.models.Transferencia;
+import com.example.Transfer_UY.Demo_1V.repository.EquipoRepository;
+import com.example.Transfer_UY.Demo_1V.repository.JugadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@Controller
+public class TransferenciaController {
+
+    @Autowired
+    private JugadorRepository jugadorRepository;
+
+    @Autowired
+    private EquipoRepository equipoRepository;
+
+    @GetMapping("/transferir")
+    public String mostrarFormularioTransferencia(Model model) {
+        model.addAttribute("equipos", equipoRepository.findAll());
+        model.addAttribute("jugadores", jugadorRepository.findAll());
+        model.addAttribute("transferencia", new Transferencia());
+        return "transferencia";
+    }
+
+    @PostMapping("/transferir")
+    public String transferirJugador(@ModelAttribute Transferencia transferencia) {
+        Optional<Jugador> jugadorOpt = jugadorRepository.findById(transferencia.getJugadorId());
+        Optional<Equipo> equipoDestinoOpt = equipoRepository.findById(transferencia.getEquipoDestinoId());
+
+        if (jugadorOpt.isPresent() && equipoDestinoOpt.isPresent()) {
+            Jugador jugador = jugadorOpt.get();
+            Equipo equipoDestino = equipoDestinoOpt.get();
+
+            jugador.setEquipo(equipoDestino);
+            jugador.setValorTransferencia(transferencia.getValorTransferencia());
+            jugador.setTipoTransferencia(transferencia.getTipoTransferencia());
+            jugadorRepository.save(jugador);
+
+            return "redirect:/jugadores";
+        } else {
+            return "redirect:/error";
+        }
+    }
+}
